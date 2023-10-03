908f81b209d111b07b9f2f1ea54888b183f0c599
Felix Han <felix.han@assurity.co.nz>
2023-10-04 04:43:30 +1300

    Added unit tests

---
+++ Backend/TodoList.Api/TodoList.Api.UnitTests/TodoItemServiceTests.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TodoList.Data.Entities;
+using TodoList.Data.Interfaces;
+using TodoList.Service.Services;
+using Xunit;
+
+namespace TodoList.Api.UnitTests
+{
+    public class TodoItemServiceTests
+    {
+        private readonly Mock<ILogger<TodoItem>> todoItemLoggerMock;
+        private readonly Mock<ITodoItemRepository> todoItemRepositoryMock;
+        private readonly TodoItemService todoItemService;
+
+
+        public TodoItemServiceTests() {
+
+            todoItemLoggerMock = new Mock<ILogger<TodoItem>>();
+            todoItemRepositoryMock = new Mock<ITodoItemRepository>();
+            todoItemService = new TodoItemService( todoItemLoggerMock.Object,todoItemRepositoryMock.Object );
+        }
+
+        [Fact]
+        public async void GetListAsync_ShouldReturnAListOfTodoItems_WhenTodoItemsExist() {
+            var todoItems = CreateTodoItemList();
+
+            todoItemRepositoryMock.Setup( c => c.GetListAsync(It.IsAny<Expression<Func<TodoItem, bool>>>())).ReturnsAsync( todoItems );
+
+            var result = await todoItemService.GetListAsync();
+
+            Assert.NotNull( result );
+            Assert.IsType<List<TodoItem>>( result );
+        }
+
+        [Fact]
+        public async void GetListAsync_ShouldReturnEmptyList_WhenNoTodoItemExists() {
+            todoItemRepositoryMock.Setup( c => c.GetListAsync(It.IsAny<Expression<Func<TodoItem, bool>>>()) )
+                .ReturnsAsync( (List<TodoItem>)null );
+
+            var result = await todoItemService.GetListAsync();
+
+            Assert.Null( result );
+        }
+
+        [Fact]
+        public async void GetListAsync_ShouldCallGetListAsyncFromRepository_OnlyOnce() {
+            todoItemRepositoryMock.Setup( c => c.GetListAsync(It.IsAny<Expression<Func<TodoItem, bool>>>()))
+               .ReturnsAsync( new List<TodoItem>());
+
+            await todoItemService.GetListAsync();
+
+            todoItemRepositoryMock.Verify( mock => mock.GetListAsync(It.IsAny<Expression<Func<TodoItem, bool>>>()), Times.Once );
+        }
+
+        [Fact]
+        public async void GetOneAsyncById_ShouldReturnTodoItem_WhenTodoItemExist() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.Setup( c => c.GetOneAsync(todoItem.Id) )
+               .ReturnsAsync(todoItem);
+
+            var result = await todoItemService.GetOneAsync( todoItem.Id );
+
+            Assert.NotNull( result );
+            Assert.IsType<TodoItem>( result );
+        }
+
+        [Fact]
+        public async void GetOneAsyncById_ShouldReturnNull_WhenTodoItemNotExists() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.Setup( c => c.GetOneAsync(new Guid("cb53a06e-1dbf-4b47-a987-7b47e9f02eb9")))
+              .ReturnsAsync( (TodoItem)null );
+
+            var result = await todoItemService.GetOneAsync(new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9"));
+
+            Assert.Null( result );
+        }
+
+        [Fact]
+        public async void GetOneAsyncById_ShouldCallGetOneAsyncByIdFromRepository_OnlyOnce() {
+            todoItemRepositoryMock.Setup( c => c.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ) )
+                .ReturnsAsync( new TodoItem() );
+
+            await todoItemService.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) );
+
+            todoItemRepositoryMock.Verify( mock => mock.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ), Times.Once );
+        }
+
+        [Fact]
+        public async void Create_ShouldCreateTodoItem_WhenTodoItemDescriptionDoesNotExist() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.Setup( c => c.CreateAsync( todoItem )).ReturnsAsync( todoItem );
+
+
+            var result = await todoItemService.CreateAsync( todoItem );
+
+            Assert.NotNull( result );
+            Assert.IsType<TodoItem>( result );
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Create_ShouldNotCreateTodoItem_WhenTodoItemDescriptionExist() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.Setup( c => c.GetExistsAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( true );
+
+            await Assert.ThrowsAnyAsync<InvalidOperationException>( () => todoItemService.CreateAsync( todoItem ) );
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Create_ShouldCallCreateAsyncFromRepository_OnlyOnce() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.Setup( c => c.CreateAsync( todoItem ) ).ReturnsAsync( todoItem );
+
+
+            var result = await todoItemService.CreateAsync( todoItem );
+
+            todoItemRepositoryMock.Verify( mock => mock.CreateAsync( todoItem ), Times.Once );
+        }
+
+        [Fact]
+        public async void Update_ShouldUpdateTodoItem_WhenTodoItemExistAndNoDuplicateDescription() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.SetupSequence( c => c.GetExistsAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( true ).ReturnsAsync( false );
+            todoItemRepositoryMock.Setup( c => c.UpdateAsync( todoItem ) ).ReturnsAsync( todoItem );
+
+
+            var result = await todoItemService.UpdateAsync( todoItem );
+
+            Assert.NotNull( result );
+            Assert.IsType<TodoItem>( result );
+        }
+
+        [Fact]
+        public async void Update_ShouldNotUpdateTodoItem_WhenTodoItemNotExists() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.SetupSequence( c => c.GetExistsAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( false );
+
+            await Assert.ThrowsAnyAsync<DbUpdateConcurrencyException>( () => todoItemService.UpdateAsync( todoItem ) );
+        }
+
+        [Fact]
+        public async void Update_ShouldNotUpdateTodoItem_WhenTodoItemExistsButDuplicateDescriptionExists() {
+            var todoItem = CreateTodoItem();
+
+            todoItemRepositoryMock.SetupSequence( c => c.GetExistsAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( true ).ReturnsAsync( true );
+
+            await Assert.ThrowsAnyAsync<InvalidOperationException>( () => todoItemService.UpdateAsync( todoItem ) );
+        }
+
+        private List<TodoItem> CreateTodoItemList() {
+            return new List<TodoItem> {
+                new TodoItem { Id = new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9"), Description = "This is my first Todo Item", IsCompleted = false },
+                new TodoItem {Id = new Guid( "db5437bb-6d7c-4b9f-81cc-129b9da2c725"), Description = "This is my second Todo Item", IsCompleted = false }
+            };
+        }
+
+        private TodoItem CreateTodoItem() {
+            return new TodoItem() {
+                Id = new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ),
+                Description = "This is my first Todo Item",
+                IsCompleted = false
+            };
+        }
+    }
+}
---
+++ Backend/TodoList.Api/TodoList.Api.UnitTests/TodoItemsControllerTest.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TodoList.Api.Controllers;
+using TodoList.Api.Mapping;
+using TodoList.Api.Model;
+using TodoList.Data.Entities;
+using TodoList.Service.Interfaces;
+using Xunit;
+
+namespace TodoList.Api.UnitTests
+{
+    public class TodoItemsControllerTest
+    {
+        private readonly Mock<ILogger<TodoItemsController>> todoItemLoggerMock;
+        private readonly Mock<ITodoItemService> todoItemServiceMock;
+        private readonly TodoItemsController controller;
+
+        public TodoItemsControllerTest() {
+            Bootstrapper.Bootstrap();
+            this.todoItemLoggerMock = new Mock<ILogger<TodoItemsController>>();
+            this.todoItemServiceMock = new Mock< ITodoItemService >();
+            this.controller = new TodoItemsController( todoItemServiceMock.Object, todoItemLoggerMock.Object );
+        }
+
+        [Fact]
+        public async void GetTodoItems_ShouldReturnOk_WhenTodoItemsExist() {
+            var todoItems = CreateTodoItemList();
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetListAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( todoItems );
+
+            var result = await controller.GetTodoItems();
+
+            // Assert
+            Assert.IsType<OkObjectResult>( result );
+        }
+
+        [Fact]
+        public async void GetTodoItem_ShouldReturnOk_WhenTodoItemsNotExist() {
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetListAsync(It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( new List<TodoItem>() );
+
+            var result = await controller.GetTodoItems();
+
+            // Assert
+            Assert.IsType<OkObjectResult>( result );
+        }
+
+        [Fact]
+        public async void GetTodoItems_ShouldCallGetListAsync_OnlyOnce() {
+            var todoItems = CreateTodoItemList();
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetListAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ) ).ReturnsAsync( todoItems );
+
+            var result = await controller.GetTodoItems();
+
+            // Assert
+            todoItemServiceMock.Verify( mock => mock.GetListAsync( It.IsAny<Expression<Func<TodoItem, bool>>>() ), Times.Once );
+        }
+
+        [Fact]
+        public async void GetTodoItem_ShouldReturn_WhenTodoItemExist() {
+            var todoItem = CreateTodoItem();
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ) ).ReturnsAsync( todoItem );
+
+            var result = await controller.GetTodoItem( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) );
+
+            // Assert
+            Assert.IsType<OkObjectResult>( result );
+        }
+
+        [Fact]
+        public async void GetTodoItem_ShouldNotReturn_WhenTodoItemNotExist() {
+            var todoItem = CreateTodoItem();
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ) ).ReturnsAsync( (TodoItem)null );
+
+            var result = await controller.GetTodoItem( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ) as ObjectResult;
+
+            // Assert
+            Assert.Equivalent( 404, result.StatusCode);
+        }
+
+        [Fact]
+        public async void GetTodoItem_ShouldCallGetOneAsync_OnlyOnce() {
+            var todoItem = CreateTodoItem();
+
+            // Arrange
+            todoItemServiceMock.Setup( c => c.GetOneAsync( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) ) ).ReturnsAsync( todoItem );
+
+            var result = await controller.GetTodoItem( new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ) );
+
+            // Assert
+            todoItemServiceMock.Verify( mock => mock.GetOneAsync(new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" )), Times.Once );
+        }
+
+
+        //[Fact]
+        //public async void Create_ShouldReturnOk_WhenTodoItemCreated() {
+        //    var todoItem = CreateTodoItem();
+        //    var todoItemDto = new TodoItemDto() { Description = todoItem.Description };
+
+        //    todoItemServiceMock.Setup( c => c.CreateAsync( todoItem ) ).ReturnsAsync( todoItem );
+        //    todoItemServiceMock.CallBase = false;
+        //    var result = await controller.PostTodoItem( todoItemDto );
+
+        //    Assert.IsType<OkObjectResult>( result );
+        //}
+
+        private List<TodoItem> CreateTodoItemList() {
+            return new List<TodoItem> {
+                new TodoItem { Id = new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9"), Description = "This is my first Todo Item", IsCompleted = false },
+                new TodoItem {Id = new Guid( "db5437bb-6d7c-4b9f-81cc-129b9da2c725"), Description = "This is my second Todo Item", IsCompleted = false }
+            };
+        }
+
+        private TodoItem CreateTodoItem() {
+            return new TodoItem() {
+                Id = new Guid( "cb53a06e-1dbf-4b47-a987-7b47e9f02eb9" ),
+                Description = "This is my first Todo Item",
+                IsCompleted = false
+            };
+        }
+    }
+}
